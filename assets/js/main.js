e6ac47fc3526bad62897ec5b82a36457ba62b8b9
Jenseko <jenscode23@gmail.com>
2023-09-01 00:43:01 +0200

    make some adjustments

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,45 +8,57 @@ let pointsCom = 0;
 
 mainScore.innerHTML = `${pointsMe} : ${pointsCom}`;
 
+const restartGame = () => {
+  playedRounds = 0;
+  pointsMe = 0;
+  pointsCom = 0;
+  mainScore.innerHTML = `${pointsMe} : ${pointsCom}`;
+  changeText.innerHTML = "let's play";
+  displayRounds.innerHTML = `${playedRounds} / ${rounDs}`;
+};
+
 const letsPlay = (userChoice) => {
   let rounDs = document.querySelector('input[name="round"]:checked').value;
 
   if (playedRounds < rounDs) {
     playedRounds++;
 
-    const commandS = ["stone", "paper", "scissor"];
+    const commandS = ["Stone", "Paper", "Scissor"];
     let comsChoice = commandS[Math.floor(Math.random() * 3)];
 
     let resultText;
 
     if (userChoice === comsChoice) {
-      resultText = "Draw!";
+      resultText = "It's a Draw !";
+      changeText.innerHTML = `You both choose ${userChoice} <br><br> ${resultText}`;
     } else if (
-      (userChoice === "stone" && comsChoice === "scissor") ||
-      (userChoice === "paper" && comsChoice === "stone") ||
-      (userChoice === "scissor" && comsChoice === "paper")
+      (userChoice === "Stone" && comsChoice === "Scissor") ||
+      (userChoice === "Paper" && comsChoice === "Stone") ||
+      (userChoice === "Scissor" && comsChoice === "Paper")
     ) {
-      resultText = "You Win!";
+      resultText = "You wins !";
       pointsMe++;
+      changeText.innerHTML = `${userChoice} (YOU) beats ${comsChoice} (Com) <br><br> ${resultText}`;
     } else {
-      resultText = "You Lose!";
+      resultText = "You lose !";
       pointsCom++;
+      changeText.innerHTML = `${comsChoice} (Com) beats ${userChoice} (YOU) <br><br> ${resultText}`;
     }
 
     // Update score text
-    mainScore.innerHTML = `${pointsMe} : ${pointsCom} (com) ${resultText}`;
-    changeText.innerHTML = `${userChoice} (YOU) ${comsChoice} (COM) ${resultText}`;
+    mainScore.innerHTML = `${pointsMe} : ${pointsCom}`;
 
     // Check game result
     if (playedRounds == rounDs) {
       if (pointsMe > pointsCom) {
-        changeText.innerHTML = "You Win!";
+        changeText.innerHTML = "The User Wins !";
       } else if (pointsMe < pointsCom) {
-        changeText.innerHTML = "You Lose!";
+        changeText.innerHTML = "The User Lose !";
       } else {
-        changeText.innerHTML = "It's a Quitgame!";
+        changeText.innerHTML = "It's a Quitgame !";
       }
     }
   }
+  form.style.display = "none";
   displayRounds.innerHTML = `${playedRounds} / ${rounDs}`;
 };
